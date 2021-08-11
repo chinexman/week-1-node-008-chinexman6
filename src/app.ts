39e3b6829c8a44910a51a7d85e35cdf224deb8df
chinedu <chinedu.emordi@decagon.dev>
2021-08-11 11:39:56 +0100

    done task 6

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from 'express';
+import createError from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+
+var app = express();
+app.use(express.json());
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+import indexRouter from './routes/index';
+import usersRouter from './routes/companys';
+
+app.use('/api/companys', usersRouter);
+
+// app.get('/api/companys', function (req:Request, res:Response) {
+//   companys.sort((a,b)=>a.id - b.id);
+//   console.log(companys);
+//   res.status(200).json(companys);
+// })
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req:Request, res:Response,next:any) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err:any,req:Request, res:Response,next:any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+// export default app;
---
+++ src/routes/companys.ts
@@ -0,0 +1,126 @@
+import express from 'express';
+const app = express();
+import Joi, { exist } from 'joi';
+const router = express.Router();
+import fs  from 'fs';
+
+import companys from '/Users/decagon/Desktop/NodeAsign/week-6-node-008-chinexman/src/routes/database.json';
+
+
+app.use(express.json());
+ 
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+// let companys :any
+// fs.exists('/Users/decagon/Desktop/NodeAsign/week-6-node-008-chinexman/src/routes/database.json',(exists)=>{
+//   if(exists){
+//     companys = fs.readFileSync('/Users/decagon/Desktop/NodeAsign/week-6-node-008-chinexman/src/routes/database.json');
+//   }else{
+//     companys = [];
+//   }
+// })
+
+router.get('/', function (req, res,next) {
+  companys.sort((a:{id:number},b:{id:number})=>a.id - b.id);
+  //console.log(companys);
+  res.status(200).json(companys);
+})
+
+
+router.get('/:id', function (req, res) {
+  const company = companys.find((c:{id:number})=>c.id ===parseInt(req.params.id))
+  if(!company) return res.status(404).send('The company with the given id was not found');
+res.send(company);
+})
+
+router.post('/', function(req, res){
+
+  
+
+
+  if(!req.body.organization){
+    return res.status(400).send('Organisation Name is required');
+  }
+  console.log(req.body);
+// console.log(req.body.organization.length);
+const company ={
+id: companys.length + 1,
+organization: req.body.organization,
+createdAt:new Date().toISOString(),
+updatedAt: new Date().toISOString(),
+products: req.body.products,
+marketValue: req.body.marketValue,
+address: req.body.address,
+ceo: req.body.ceo,
+country: req.body.country,
+
+noOfEmployees:req.body.noOfEmployees,
+employees:req.body.employees
+}
+companys.push(company);
+fs.writeFileSync("/Users/decagon/Desktop/NodeAsign/week-6-node-008-chinexman/src/routes/database.json", JSON.stringify(companys, null, " ") );
+
+res.status(201).send(company);
+
+})
+
+
+
+router.put('/:id', function(req, res){
+  // const result = validateCompany(req.body);
+//const { error} = validateCompany(req.body);
+// if(error){
+ // return res.status(400).send(error.details[0].message);
+//}
+
+  const  updateCompany = companys.find((c:{id:number})=>c.id ===parseInt(req.params.id))
+  if(!updateCompany) return res.status(404).send('The company with the given id was not found');
+// res.send(company);
+console.log(req.body);
+
+updateCompany.organization= req.body.organization || updateCompany.organization,
+updateCompany.updatedAt=new Date().toISOString(),
+updateCompany.products=req.body.products || updateCompany.products,
+updateCompany.marketValue=req.body.marketValue || updateCompany.marketValue,
+updateCompany.address = req.body.address || updateCompany.address,
+updateCompany.ceo =req.body.ceo || updateCompany.ceo,
+updateCompany.country = req.body.country || updateCompany.country,
+
+updateCompany.noOfEmployees = req.body.noOfEmployees || updateCompany.noOfEmployees,
+updateCompany.employees =req.body.employees || updateCompany.employees
+
+companys.push(updateCompany);
+//fs.writeFileSync("/Users/decagon/Desktop/NodeAsign/week-6-node-008-chinexman/src/routes/database.json", JSON.stringify(companys, null, " ") );
+
+res.status(201).json(updateCompany);
+
+});
+
+
+router.delete('/:id', function (req, res) {
+  const deleteCompany = companys.find((c:{id:number})=>c.id ===parseInt(req.params.id))
+  if(!deleteCompany) return res.status(404).send('The company with the given id was not found');
+
+  const index = companys.indexOf(deleteCompany);
+  companys.splice(index,1)
+res.send(deleteCompany);
+})
+
+// function validateCompany(company:any){
+//   const schema ={
+//       organisation:Joi.string().min(3).required()
+//      }
+//      return  Joi.valid(company, schema);
+
+// }
+
+//module.exports = router;
+export default router
+  //
+// const result = validateCompany(req.body);
+//const { error} = validateCompany(req.body);
+// if(error){
+ // return res.status(400).send(error.details[0].message);
+//}
